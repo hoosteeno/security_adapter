853be87ebface36cf3a8d147ac09bfebbf929b9f
hoosteeno <justin@tentacu.com>
2013-06-13 17:09:05 -0600

    mostly parsing everything

--- get_json_from_announce.py
+++ get_json_from_announce.py
@@ -1,45 +1,60 @@
+import time
 from bs4 import BeautifulSoup
 
-advisories = BeautifulSoup(open("known-vulnerabilities/firefox.html"))
+advisories = BeautifulSoup(open("announce/index.html"))
 
 for link in advisories.find_all('a'):
     # create a new advisory now
     # path = href, title = text
     href = link.get('href')
     if "announce" in href:
-        filename = href.replace('/security/', '')
-        text = link.get_text()
 
+        filename = href.replace('/security/', '')
+
+        #TODO break data out into a model
         # make_advisory_from_file href
         advisory = BeautifulSoup(open(filename))
         data = {}
+
         data['headline'] = advisory.find('h1').get_text()
+        data['id'] = link.get_text().replace('MFSA ', '')
+
         meta = advisory.find('h1').find_next_sibling('p')
 
-        #TODO make these work
-        #description = advisory.find(text="Description").find_next_sibling('p')
-        #references = advisory.find(text="References").find_next_siblings('p')
+        meta_fields = {
+            'Title:': 'title',
+            'Impact:': 'impact',
+            'Announced:': 'date_s',
+            'Reporter:': 'reporter',
+            'Products:': 'products',
+            'Fixed in:': 'fixed_in'
+        }
 
-        data['title'] = meta.find(text="Title:").parent.next_sibling
-        data['impact'] = meta.find(text="Impact:").parent.next_sibling
+        for key, value in meta_fields.iteritems():
+            element = meta.find(text=key)
+            if (element): 
+                if "products" in value:
+                    data[value] = []
+                    for product in element.parent.next_sibling.split(','):
+                        data[value].append(product.strip())
+                elif "fixed_in" in value:
+                    data[value] = []
+                    data[value].append(element.parent.next_sibling.strip())
+                    for sibling in element.parent.find_next_siblings():
+                        for string in sibling.stripped_strings:
+                            data[value].append(string.strip())
+                else:
+                    data[value] = element.parent.next_sibling.strip()
 
-        #TODO make date
-        data['date_s'] = meta.find(text="Announced:").parent.next_sibling
+        data['date'] = time.strptime(data['date_s'], '%B %d, %Y')
 
-        data['reporter'] = meta.find(text="Reporter:").parent.next_sibling
+        description = advisory.find('h3', text='Description')
+        if description:
+            data['description'] = description.find_next_sibling('p')
 
-        data['products'] = meta.find(
-                text="Products:"
-            ).parent.next_sibling.split(',')
-
-        data['fixed_in'] = []
-        fin = meta.find(text="Fixed in:")
-        # this gets the first sibling, on the same line
-        data['fixed_in'].append(fin.parent.next_sibling)
-        # this gets the rest of the siblings
-        for sibling in fin.parent.find_next_siblings():
-            for string in sibling.stripped_strings:
-                data['fixed_in'].append(string)
+        references = advisory.find('h3', text="References")
+        if references:
+            data['references'] = references.find_next_siblings()[0]
 
         print data
 
